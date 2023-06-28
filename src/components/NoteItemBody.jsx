844b6a0c04f842825dbd4a202a81ee9e68c038c5
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-06-28 16:45:10 +0700

    penambahan validasi properti

--- src/components/NoteItemBody.jsx
+++ src/components/NoteItemBody.jsx
@@ -21,6 +21,7 @@ NoteItemBody.propTypes = {
   id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
 };
 
 export default NoteItemBody;
